940e044e0e3d4cfda46051113b3ad94700fa08a8
Armaan Moosani <107946049+armaanmoosani@users.noreply.github.com>
2025-08-11 16:18:25 -0500

    Update db.php

--- public_html/module5/db.php
+++ public_html/module5/db.php
@@ -1,5 +1,5 @@
 <?php
-    $mysqli = new mysqli('localhost', 'phpuser', 'a8ushd742%o43(2jx@*ujsui', 'calendar');
+    $mysqli = new mysqli('localhost', 'phpuser', 'PASSWORD_KEY', 'calendar');
     if($mysqli->connect_errno) {
         printf("Connection Failed: %s\n", $mysqli->connect_error);
         exit;
